3f539911850cf24542cdc4e4584ecfd4cfb8483c
Ivan Volkovets <batmad857@gmail.com>
2022-07-19 18:00:36 +0300

    installed webpack and all dependencies, added sources, configured webpack.config.js file

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//All JS modules and functions put here
+
+import './index.html';
+import './index.scss';
+import img from './assets/Animation.svg';
+import { mult, sum } from './modulex/calc';
+
+
+console.log(mult(2,4));
+console.log(sum(2,4));
